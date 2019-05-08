1ae7bff5fe14ba51315aac598ae74722f639553c
LIZETHVERA <lizeth.veraro@gmail.com>
2019-05-08 14:52:18 -0700

    Including API bands in town to get the concerts in town with my own id

--- liri.js
+++ liri.js
@@ -8,13 +8,12 @@ var spotify = new Spotify(keys.spotify);
 
 var axios = require("axios");
 
-
+//-------------ISSUES--------------------------------//
 // concert-this
-// spotify-this-song
-// movie-this
 // do-what-it-says
+// Pending to put in case the song does not exist. 
 
-// var command = process.argv[2];
+
 // How the user can search the song with space and another parameters. 
 var liriResponse = process.argv[2];
 var search = process.argv.slice(3).join(" ");
@@ -27,6 +26,10 @@ switch (liriResponse) {
   case "movie-this":
     movieInfo();
     break;
+  
+    case "concert-this":
+    bandsInTown();
+    break;  
 };
 
 
@@ -37,19 +40,19 @@ function spotifySong() {
   if (!search) {
     // If the variable is diferent to search the variable now has the "Mr NoBody Name"
     search = "the day that never comes metallica";
-    
+
     console.log("---> You have not entered any songs, we recommend THE DAY THAT NEVER COMES BY METALLICA!");
     console.log("---> These are the first three versions. Enjoy");
-   
+
     console.log("\n-------------\n");
-  } else if (search){
-    
+  } else if (search) {
+
     // Print in terminal a message for the user
-     console.log("---> These are the first three versions. Enjoy")
-     console.log("---> If you want a specific song try puting the artist name too:")
-     console.log("\n-------------\n");
+    console.log("---> These are the first three versions. Enjoy")
+    console.log("---> If you want a specific song try puting the artist name too:")
+    console.log("\n-------------\n");
   };
- 
+
   spotify.search({ type: 'track', query: search, limit: 3 }, function (err, data) {
     if (err) {
       return console.log('Error occurred: ' + err);
@@ -59,9 +62,6 @@ function spotifySong() {
     // The main JSON Object
     var trackInfo = data.tracks.items;
     // console.log(JSON.stringify(trackInfo,null, 3));
-
-   
-
 
     // For to iterate over the object length and bring the different options.
     for (var i = 0; i < trackInfo.length; i++) {
@@ -77,9 +77,9 @@ function spotifySong() {
       console.log("Preview Link: " + previewLink);
       console.log("\n-------------\n");
     }
-    
+
   });
-  
+
 
 };
 
@@ -89,26 +89,42 @@ function movieInfo() {
   if (!search) {
     // If the variable is diferent to search the variable now has the "Mr NoBody Name"
     search = "Mr Nobody";
-    console.log("\n-------------\n");
-    console.log("---> You have not entered any movies, we recommend Mr Nobody!");
-    console.log("---> If you want you can search for a different movie by its name");
-    console.log("\n-------------\n");
+
+    console.log("\n---> If you want you can search for a different movie by its name");
+    console.log("\n---> You have not entered any movies, we recommend Mr Nobody!\n");
+  } else if (search) {
+
+    // Print in terminal a message for the user
+
+    console.log("\n ---> Here is the info for the movie you entered\n");
   };
- 
-    axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(
-      function (response) {
-        // console.log(response);
-        // console.log(response.data.Ratings);
-        console.log("\n-------------\n");
-        console.log("Title of the movie:  " + response.data.Title);
-        console.log("year came out: " + response.data.Year);
-        console.log("IMDB Rating:  " + response.data.Ratings[0].Value);
-        console.log("Rotten Tomatoes:  " + response.data.Ratings[1].Value);
-        console.log("Country:  " + response.data.Country);
-        console.log("Language:  " + response.data.Language);
-        console.log("Plot:  " + response.data.Plot);
-        console.log("Actors: " + response.data.Actors);
-      }
-    );
+
+  axios.get("http://www.omdbapi.com/?t=" + search + "&y=&plot=short&apikey=trilogy").then(
+    function (response) {
+      // console.log(response);
+      // console.log(response.data.Ratings);
+
+      console.log("Title of the movie:  " + response.data.Title);
+      console.log("year came out: " + response.data.Year);
+      console.log("IMDB Rating:  " + response.data.Ratings[0].Value);
+      console.log("Rotten Tomatoes:  " + response.data.Ratings[1].Value);
+      console.log("Country:  " + response.data.Country);
+      console.log("Language:  " + response.data.Language);
+      console.log("Plot:  " + response.data.Plot);
+      console.log("Actors: " + response.data.Actors);
+    }
+  );
 
 };
+
+function bandsInTown() {
+
+  axios.get("https://rest.bandsintown.com/artists/" + search + "/events?app_id=943e8f5b-037a-453f-84bd-a9aa28593784").then(
+    function(response) {
+      
+      console.log(JSON.stringify(response.data, null, 2));
+    
+    }
+  );
+};
+
